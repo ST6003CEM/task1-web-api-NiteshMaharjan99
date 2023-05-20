143926188ab42a1a1bda6380bb02f0caa58f6d7c
NiteshMaharjan99 <niteshmaharjan100@gmail.com>
2023-05-20 11:57:10 +0545

    Schema created

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+
+const mongoose = require('mongoose')
+
+const app = express()
+
+mongoose.connect('mongodb://127.0.0.1:27017/restaurantAPI')
+.then(() => console.log('Connected to mongoDB server'))
+.catch((err) => console.log(err))
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send("Hello world")   
+})
+
+
+// Error handling
+app.use((err, req, res, next) => {
+    console.error(err)
+    if (err.name === 'ValidationError') res.status(400)
+    else if (err.name === 'CastError') res.status(400)
+    console.log(err.message)
+    res.json({ error: err.message })
+})
+
+app.use((req, res) => {
+    res.status(404).json({ error: "Path Not Found" })
+})
+
+app.listen(3000,() => {
+    console.log('server is running on port 3000')
+})
---
+++ model/RestaurantPOS.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose');
+
+const restaurantSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  tables: [
+    {
+      number: {
+        type: Number,
+        required: true,
+      },
+      capacity: {
+        type: Number,
+        required: true,
+      },
+      status: {
+        type: String,
+        enum: ['vacant', 'occupied'],
+        default: 'vacant',
+      },
+    },
+  ],
+  menu: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      category: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  orders: [
+    {
+      tableNumber: {
+        type: Number,
+        required: true,
+      },
+      items: [
+        {
+          name: {
+            type: String,
+            required: true,
+          },
+          price: {
+            type: Number,
+            required: true,
+          },
+          quantity: {
+            type: Number,
+            required: true,
+          },
+        },
+      ],
+      totalAmount: {
+        type: Number,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+      completed: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  ],
+});
+
+const Restaurant = mongoose.model('Restaurant', restaurantSchema);
+
+module.exports = Restaurant;
