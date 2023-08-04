5377469461ad1813d99ef7c897e0a80fe798d7dd
NiteshMaharjan99 <niteshmaharjan100@gmail.com>
2023-08-04 13:25:14 +0545

    user test cases added

--- index.js
+++ index.js
@@ -1,49 +1,5 @@
-const express = require('express')
 
-const mongoose = require('mongoose')
-
-const app = express()
-
-const restaurant_routes = require('./routes/restaurant_routes')
-
-const user_routes = require('./routes/user_routes')
-
-const {verifyUser} = require('./middleware/auth')
-
-const upload = require('./middleware/upload')
-
-
-mongoose.connect('mongodb://127.0.0.1:27017/restaurantAPI')
-.then(() => console.log('Connected to mongoDB server'))
-.catch((err) => console.log(err))
-
-app.use(express.json())
-app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-    res.send("Hello world")   
-})
-
-app.use('/restaurants',verifyUser ,restaurant_routes)
-app.use('/users', user_routes)
-
-
-app.post('/images', upload.single('photo'), (req,res) => {
-    res.json(req.file)
-})
-
-
-app.use((err, req, res, next) => {
-    console.error(err)
-    if (err.name === 'ValidationError') res.status(400)
-    else if (err.name === 'CastError') res.status(400)
-    console.log(err.message)
-    res.json({ error: err.message })
-})
-
-app.use((req, res) => {
-    res.status(404).json({ error: "Path Not Found" })
-})
+const app = require ('./app')
 
 app.listen(3000,() => {
     console.log('server is running on port 3000')
---
+++ tests/uesrs.test.js
@@ -0,0 +1,66 @@
+const supertest = require('supertest')
+const app = require('../app')
+const { default: mongoose } = require('mongoose')
+const User = require('../model/User')
+
+const api = supertest(app)
+
+beforeAll(async () => {
+    await User.deleteMany({})
+})
+
+test('User can be registerd', async () => {
+    await api.post('/users/register')
+        .send({
+            username: 'testUser1',
+            password: 'test123',
+            fullname: "TestUser"
+        }).expect(201)
+})
+
+test('Duplicate Username cannot be registered', async () => {
+    res = await api.post('/users/register')
+        .send({
+            username: 'testUser1',
+            password: 'test123',
+            fullname: 'TestUser'
+        }).expect(400)
+    expect(res.body.error).toBeDefined()
+    expect(res.body.error).toMatch(/already registered/)
+})
+
+test('missing fullname cannot be registered', async () => {
+    res = await api.post('/users/register')
+        .send({
+            username: 'testUser3',
+            password: 'test123'
+        }).expect(400)
+    expect(res.body.error).toBeDefined()
+    expect(res.body.error).toMatch(/validation failed/)
+})
+
+describe('Register user can login ', () => {
+    test('User logged in', async () => {
+        await api.post('/users/login')
+            .send({
+                username: 'testUser1',
+                password: 'test123'
+            }).expect(200)
+    })
+
+
+    test('Unregistered User cannot be logged in', async () => {
+        await api.post('/users/login')
+            .send({
+                username: 'testUser13',
+                password: 'test123'
+            }).expect(401)
+            expect(res.body.error).toBeDefined()
+            expect(res.body.error).toMatch(/User validation failed: fullname: Path `fullname` is required./)
+    })
+})
+
+
+afterAll(async () => {
+    await mongoose.connection.close()
+})
