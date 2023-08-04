b4cbfb0fc4b2770f469047d02e111a4b1a88d243
NiteshMaharjan99 <niteshmaharjan100@gmail.com>
2023-08-04 18:33:10 +0545

    Order model added

---
+++ model/Order.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema(
+    {
+        // Reference to the user who placed the order
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        // Array of products in the order
+        menus: [
+            {
+                menu: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: 'Menu',
+                    required: true,
+                },
+                quantity: {
+                    type: Number,
+                    required: true,
+                    min: 1,
+                },
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
